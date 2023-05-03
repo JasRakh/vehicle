65a5c4eb0ba7f54f064e21e680ea2800e23f7174
JasRakh <75082230+JasRakh@users.noreply.github.com>
2023-05-03 19:14:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+nextEl.addEventListener(()=>{
+  
+})
